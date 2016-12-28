d800174de4fa9d6717c62be11bd94484eb586fd0
BenjiBackslash <hanan888@gmail.com>
2016-12-28 14:47:39 +0200

    TestDaemon

--- intsights/task/test/TestDaemon.py
+++ intsights/task/test/TestDaemon.py
@@ -1,20 +1,39 @@
 import unittest
+import procname
 
 from intsights.task.main import MyDaemon
 from intsights.task.runner import Runner
 
 class TestDaemon(unittest.TestCase):
+    @staticmethod
+    def make_daemon_name():
+        import uuid
+        return 'MyDaemonTest' + str(uuid.uuid4()).replace('-','')[0:7]
     def setUp(self):
         unittest.TestCase.setUp(self)
+        procname.setprocname(self.make_daemon_name())
         self.runner = MockRunner()  
         self.mydaemon = MyDaemon(self.runner, 1)
         self.mydaemon.run()
-    def test_run(self):
-        #check if it does run
-        pass
         
-    def testName(self):
-        return 'TestDaemon'
+        
+    def testUp(self):
+        #look at the linux processes and find the daemon
+        #what will be its name ?
+        
+        
+    def test(self):
+        #check if it does run
+        print('what')
+#         self.assertEqual(0, 1, "sdsa")
+# 
+#     def test2(self):
+#         #check if it does run
+#         print('what')
+#     #         self.assertEqual(0, 1, "sdsa")
+        
+#     def testName(self):
+#         return 'TestDaemon'
     
     def tearDown(self):
         unittest.TestCase.tearDown(self)
